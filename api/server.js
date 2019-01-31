5d144106060a5a82676997f89945032151207208
wisdomtooth <cbtsao47@gmail.com>
2019-01-31 17:20:35 -0600

    add post endpoint check

--- api/server.js
+++ api/server.js
@@ -6,19 +6,19 @@ const db = require("../data/dbConfig");
 configMiddleware(server);
 
 server.get("/", async (req, res) => {
-  res.status(200).send("sanity check");
+  res.status(200).json({ message: "sanity check" });
 });
 
 server.get("/hobbits", async (req, res) => {
   const hobbits = await db("hobbits");
-  console.log(hobbits);
+  //   console.log(hobbits);
   res.status(200).json(hobbits);
 });
 
 server.post("/hobbits", async (req, res) => {
   const newHobbit = req.body;
   const result = await db("hobbits").insert(newHobbit);
-  res.status(200).json(result);
+  res.status(201).json(result);
 });
 
 server.delete("/hobbits/:id", async (req, res) => {
---
+++ api/server.spec.js
@@ -0,0 +1,52 @@
+const request = require("supertest");
+const db = require("../data/dbConfig");
+const server = require("./server");
+afterEach(async () => {
+  await db("hobbits").truncate();
+});
+
+describe("server.js", () => {
+  describe("GET/endpoint", () => {
+    it("should respond with status code 200 ok", async () => {
+      let res = await request(server).get("/");
+
+      expect(res.status).toBe(200);
+    });
+    it("should respond with JSON", async () => {
+      let res = await request(server).get("/");
+      expect(res.type).toMatch(/json/i);
+    });
+  });
+  describe("GET/hobbits endpoint", () => {
+    it("should respond with status code 200 ok", async () => {
+      let res = await request(server).get("/hobbits");
+      expect(res.status).toBe(200);
+    });
+    it("should respond with JSON", async () => {
+      let res = await request(server).get("/hobbits");
+      expect(res.type).toMatch(/json/i);
+    });
+    it("should respond with an array", async () => {
+      let expected = [];
+      let res = await request(server).get("/hobbits");
+      expect(res.body).toEqual(expected);
+    });
+  });
+  describe("POST/hobbits endpoint", () => {
+    it("should respond with status code 201 created", async () => {
+      let body = { name: "random dwarf" };
+      let res = await request(server)
+        .post("/hobbits")
+        .send(body);
+      expect(res.status).toBe(201);
+    });
+    it("should return the id of the created", async () => {
+      let body = { name: "random dwarf" };
+      let res = await request(server)
+        .post("/hobbits")
+        .send(body);
+      // k
+      expect(res.body).toEqual([1]);
+    });
+  });
+});
