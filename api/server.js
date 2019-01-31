f123c5d7fbd1320d2bdd3ba85b6c1599d509dabd
wisdomtooth <cbtsao47@gmail.com>
2019-01-31 16:47:44 -0600

    add get post delete routes

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const configMiddleware = require("../config/middleware");
 const server = express();
+const db = require("../data/dbConfig");
 
 configMiddleware(server);
 
@@ -8,4 +9,23 @@ server.get("/", async (req, res) => {
   res.status(200).send("sanity check");
 });
 
+server.get("/hobbits", async (req, res) => {
+  const hobbits = await db("hobbits");
+  console.log(hobbits);
+  res.status(200).json(hobbits);
+});
+
+server.post("/hobbits", async (req, res) => {
+  const newHobbit = req.body;
+  const result = await db("hobbits").insert(newHobbit);
+  res.status(200).json(result);
+});
+
+server.delete("/hobbits/:id", async (req, res) => {
+  const { id } = req.params;
+  const numberDeleted = await db("hobbits")
+    .where({ id })
+    .del();
+  res.status(200).json(numberDeleted);
+});
 module.exports = server;
